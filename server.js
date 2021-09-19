66e42bb20d59e59476a51b8cf96680171e2d740e
Muneeza Leslie <sebagabomuneeza@gmail.com>
2021-09-19 20:28:36 +0300

    atlas connection

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/bus", {
+  .connect(process.env.MONGO_URL || "mongodb://127.0.0.1:27017/bus", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
